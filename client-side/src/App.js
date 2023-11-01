75701e9822a4502b6d58c86a92264fd18569ecea
Abhithecoder03 <107066993+Abhithecoder03@users.noreply.github.com>
2023-11-02 01:49:31 +0530

    Added dashboard and its route in App.js

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -8,18 +8,25 @@ import Login from "./Pages/Authentication/Login/Login";
 import TermConditions from "./Pages/Term&Conditions/TermConditions";
 import Privacy from "./Pages/Term&Conditions/Privacy";
 import Footer from "./Components/Footer/Footer";
+import StudentDashboard from "./Pages/StudentDashboard/Student";
+import Profile from "./Pages/StudentDashboard/Profile";
 const App = () => {
   return (
     <div className="w-screen h-screen flex flex-col">
       <Navbar />
+
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/Student" element={<StudentDashboard />}>
+          <Route path="profile" element={<Profile />} />
+          {/* Define other nested routes here */}
+        </Route>
         <Route path="/SignUp" Component={SignUp} />
         <Route path="/Login" Component={Login} />
         <Route path="/termConsitions" Component={TermConditions} />
         <Route path="/privacy" Component={Privacy} />
       </Routes>
-      <Footer />
+
     </div>
   );
 };
