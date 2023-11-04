004e01f753bae25a472f102dfd7ebc9ec11dda71
pratikmishra03 <pratikmishra08314@gmail.com>
2023-11-05 01:41:56 +0530

    Made Video Courses section Responsive in Student Dashboard

--- client-side/src/Pages/StudentDashboard/VideoCourses/VideoCourses.js
+++ client-side/src/Pages/StudentDashboard/VideoCourses/VideoCourses.js
@@ -1,17 +1,25 @@
 import React from "react";
 import Category from "../../../Components/Category/Category";
+import Search from "../../../Components/Search/Search";
 
 const VideoCourses = () => {
   return (
     <>
-      <div className="w-4/5 bg-grey">
+      <div className="flex flex-col w-full md:w-4/5 bg-grey items-center">
+        <div className="flex w-full justify-center mt-8">
+          <Search />
+        </div>
+        <div className="flex flex-col w-full items-center mt-4">
+          <div className="w-full md:w-4/5 border-b-2 border-gray-400"></div>
+          <Category />
+        </div>
 
-        <div className="h-full p-2 border flex-col overflow-y-auto overflow-x-hidden">
+        <div className="h-full p-2 flex-col overflow-y-auto overflow-x-hidden">
           <div className="flex-col  mt-14 ">
-            <h4>
-              Computer Science & Engineering/Information Technology Courses
+            <h4 className="ml-4">
+              Computer Science & Engineering & Information Technology Courses
             </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 hover:b ">
+            <div className="w-full flex flex-wrap mt-5 lg:ml-6 justify-center lg:justify-start hover:b ">
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
                   src="/Assets/VideoCourses/Computer.jpeg"
@@ -47,17 +55,17 @@ const VideoCourses = () => {
             </div>
           </div>
           <div className="flex-col  mt-14 ">
-            <h4 className="">
-              Electrical Engineering courses
+            <h4 className="ml-4">
+              Computer Science & Engineering/Information Technology Courses
             </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+            <div className="w-full flex flex-wrap mt-5 lg:ml-6 justify-center lg:justify-start hover:b ">
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
                   src="/Assets/VideoCourses/Computer.jpeg"
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech First Year(EE/ECE)</p>
+                <p className="font-semibold">B.tech First Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -65,7 +73,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded mr-"
                 />
-                <p className="font-semibold">B.tech Second Year(EE/ECE)</p>
+                <p className="font-semibold">B.tech Second Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -73,7 +81,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-fit rounded"
                 />
-                <p className="font-semibold">B.tech Third Year(EE/ECE)</p>
+                <p className="font-semibold">B.tech Third Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white ">
                 <img
@@ -81,22 +89,22 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech Fourth Year(EE/ECE)</p>
+                <p className="font-semibold">B.tech Fourth Year(CSE/IT)</p>
               </div>
             </div>
           </div>
           <div className="flex-col  mt-14 ">
-            <h4>
-              Mechanical Engineering Courses
+            <h4 className="ml-4">
+              Computer Science & Engineering/Information Technology Courses
             </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+            <div className="w-full flex flex-wrap mt-5 lg:ml-6 justify-center lg:justify-start hover:b ">
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
                   src="/Assets/VideoCourses/Computer.jpeg"
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+                <p className="font-semibold">B.tech First Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -104,7 +112,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded mr-"
                 />
-                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Second Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -112,7 +120,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-fit rounded"
                 />
-                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Third Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white ">
                 <img
@@ -120,22 +128,22 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Fourth Year(CSE/IT)</p>
               </div>
             </div>
           </div>
           <div className="flex-col  mt-14 ">
-            <h4>
-              Mechanical Engineering Courses
+            <h4 className="ml-4">
+              Computer Science & Engineering/Information Technology Courses
             </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+            <div className="w-full flex flex-wrap mt-5 lg:ml-6 justify-center lg:justify-start hover:b ">
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
                   src="/Assets/VideoCourses/Computer.jpeg"
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+                <p className="font-semibold">B.tech First Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -143,7 +151,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded mr-"
                 />
-                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Second Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -151,7 +159,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-fit rounded"
                 />
-                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Third Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white ">
                 <img
@@ -159,22 +167,22 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Fourth Year(CSE/IT)</p>
               </div>
             </div>
           </div>
           <div className="flex-col  mt-14 ">
-            <h4>
-              Mechanical Engineering Courses
+            <h4 className="ml-4">
+              Computer Science & Engineering/Information Technology Courses
             </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+            <div className="w-full flex flex-wrap mt-5 lg:ml-6 justify-center lg:justify-start hover:b ">
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
                   src="/Assets/VideoCourses/Computer.jpeg"
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+                <p className="font-semibold">B.tech First Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -182,7 +190,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded mr-"
                 />
-                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Second Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -190,7 +198,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-fit rounded"
                 />
-                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Third Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white ">
                 <img
@@ -198,22 +206,22 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Fourth Year(CSE/IT)</p>
               </div>
             </div>
           </div>
           <div className="flex-col  mt-14 ">
-            <h4>
-              Mechanical Engineering Courses
+            <h4 className="ml-4">
+              Computer Science & Engineering/Information Technology Courses
             </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+            <div className="w-full flex flex-wrap mt-5 lg:ml-6 justify-center lg:justify-start hover:b ">
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
                   src="/Assets/VideoCourses/Computer.jpeg"
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+                <p className="font-semibold">B.tech First Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -221,7 +229,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded mr-"
                 />
-                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Second Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
                 <img
@@ -229,7 +237,7 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-fit rounded"
                 />
-                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Third Year(CSE/IT)</p>
               </div>
               <div className="flex flex-col w-72 p-1 items-center border bg-white ">
                 <img
@@ -237,49 +245,11 @@ const VideoCourses = () => {
                   alt=""
                   className="w-96 h-42 object-contain rounded"
                 />
-                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+                <p className="font-semibold">B.tech Fourth Year(CSE/IT)</p>
               </div>
             </div>
           </div>
-          <div className="flex-col  mt-14 ">
-            <h4>
-              Mechanical Engineering Courses
-            </h4>
-            <div className="w-full flex flex-wrap mt-5 ml-6 ">
-              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
-                <img
-                  src="/Assets/VideoCourses/Computer.jpeg"
-                  alt=""
-                  className="w-96 h-42 object-contain rounded"
-                />
-                <p className="font-semibold">B.tech First Year(ME/CE)</p>
-              </div>
-              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
-                <img
-                  src="/Assets/VideoCourses/Computer.jpeg"
-                  alt=""
-                  className="w-96 h-42 object-contain rounded mr-"
-                />
-                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
-              </div>
-              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
-                <img
-                  src="/Assets/VideoCourses/Computer.jpeg"
-                  alt=""
-                  className="w-96 h-42 object-fit rounded"
-                />
-                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
-              </div>
-              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
-                <img
-                  src="/Assets/VideoCourses/Computer.jpeg"
-                  alt=""
-                  className="w-96 h-42 object-contain rounded"
-                />
-                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
-              </div>
-            </div>
-          </div>
+          
 
 
         </div>
