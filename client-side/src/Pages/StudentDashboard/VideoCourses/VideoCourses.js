acb5e8aa4a80d2ab190eb7b967545e1e1fd575e3
pratikmishra03 <pratikmishra08314@gmail.com>
2023-11-05 01:20:08 +0530

    Added Video Courses section in Student Dashboard

--- client-side/src/Pages/StudentDashboard/VideoCourses/VideoCourses.js
+++ client-side/src/Pages/StudentDashboard/VideoCourses/VideoCourses.js
@@ -1,9 +1,291 @@
-import React from 'react'
+import React from "react";
+import Category from "../../../Components/Category/Category";
 
 const VideoCourses = () => {
   return (
-    <div>VideoCourses</div>
-  )
-}
+    <>
+      <div className="w-4/5 bg-grey">
 
-export default VideoCourses
+        <div className="h-full p-2 border flex-col overflow-y-auto overflow-x-hidden">
+          <div className="flex-col  mt-14 ">
+            <h4>
+              Computer Science & Engineering/Information Technology Courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 hover:b ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(CSE/IT)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(CSE/IT)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(CSE/IT)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Fourth Year(CSE/IT)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-col  mt-14 ">
+            <h4 className="">
+              Electrical Engineering courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(EE/ECE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(EE/ECE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(EE/ECE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Fourth Year(EE/ECE)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-col  mt-14 ">
+            <h4>
+              Mechanical Engineering Courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-col  mt-14 ">
+            <h4>
+              Mechanical Engineering Courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-col  mt-14 ">
+            <h4>
+              Mechanical Engineering Courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-col  mt-14 ">
+            <h4>
+              Mechanical Engineering Courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-col  mt-14 ">
+            <h4>
+              Mechanical Engineering Courses
+            </h4>
+            <div className="w-full flex flex-wrap mt-5 ml-6 ">
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech First Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded mr-"
+                />
+                <p className="font-semibold">B.tech Second Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white mr-2 ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-fit rounded"
+                />
+                <p className="font-semibold">B.tech Third Year(ME/CE)</p>
+              </div>
+              <div className="flex flex-col w-72 p-1 items-center border bg-white ">
+                <img
+                  src="/Assets/VideoCourses/Computer.jpeg"
+                  alt=""
+                  className="w-96 h-42 object-contain rounded"
+                />
+                <p className="font-semibold">B.tech Furth Year(ME/CE)</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VideoCourses;
